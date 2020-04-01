4b138e971ec0a0d8fd30a183bde95d2ff9d5dbeb
Calo Corp <marc.calo@codeimmersives.com>
2020-04-01 14:38:32 -0400

    almost all my buttons toggle now

--- HighPFirst.js
+++ HighPFirst.js
@@ -1,4 +1,4 @@
-const HighPFirst = document.querySelector(".toggle-priorities")
+const HighPFirst = document.querySelector(".high-priority-first")
 HighPFirst.addEventListener('click', function(event) {  
     
     const state1 = "High Priority First";
@@ -7,7 +7,7 @@ if(HighPFirst.innerText == state1){
     HighPFirst.innerText = state2 
 }
 else if (HighPFirst.innerText == state2) {
-showHidePrior.innerText = state1
+HighPFirst.innerText = state1
 }
 
 })
