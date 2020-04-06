09ecd709e8f2d9c960ddd0481ce13e959c3f3274
Calo Corp <marc.calo@codeimmersives.com>
2020-04-06 10:25:36 -0400

    still lost on figuring out what functions i need to write and how to write them

--- show-and-hide-priorities.js
+++ show-and-hide-priorities.js
@@ -4,12 +4,13 @@ showHidePrior.addEventListener('click', function(event) {
     const state1 = "Show Priorities";
     const state2 = "Hide Priorities";
 if(showHidePrior.innerText == state1){
-    showHidePrior.innerText = state2 
+    showHidePrior.innerText = state2
+    currentTodos= namesAndPriorities(todos) 
 }
 else if (showHidePrior.innerText == state2) {
 showHidePrior.innerText = state1
+
 }
-refreshTodos()
 })
 
 
